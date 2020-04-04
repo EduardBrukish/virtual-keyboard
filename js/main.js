be15b54500bfd9d21057ed24e7e46264d849133d
Eduard.Brukish <eduardbrukish@gmail.com>
2020-04-05 00:13:55 +0200

    feat: add draw and init function to main.js

--- js/main.js
+++ js/main.js
@@ -1,176 +1,129 @@
-// 1
-// Write a function largestOdd(arr) that takes an array arr and returns the largest odd number in it. If there are no odd numbers, it should return a string "No odd numbers found".
+const keyboardLayout = {
+    eng: [['`', '~'], ['1', '!'], ['2', '@'], ['3', '#'], ['4', '$'], ['5', '%'], ['6', '^'], ['7', '&'], ['8', '*'], ['9', '('], ['0', ')'], ['-', '_'], ['=', '+'], 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', ['[', '{'], [']', '}'], ['\\', '|'], 'Del', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', [';', ':'], ['\'', '"'], 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', [',', '<'], ['.', '>'], ['/', '?'], '↑', 'Shift', 'Ctrl', 'Win', 'Alt', ' ', 'Alt', '←', '↓', '→', 'Ctrl'],
+    rus: ['ё', ['1', '!'], ['2', '@'], ['3', '#'], ['4', '$'], ['5', '%'], ['6', '^'], ['7', '&'], ['8', '*'], ['9', '('], ['0', ')'], ['-', '_'], ['=', '+'], 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', ['\\', '/'], 'Del', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', ['.', ','], '↑', 'Shift', 'Ctrl', 'Win', 'Alt', ' ', 'Alt', '←', '↓', '→', 'Ctrl'],
+};
+const functionalKeys = ['Backspace', 'Tab', 'Del', 'CapsLock', 'Enter', 'Shift', 'Ctrl', 'Alt'];
+const keyCodes = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8, 9, 81, 87, 69,
+    82, 84, 89, 85, 73, 79, 80, 219, 221, 220, 46, 20, 65, 83, 68, 70, 71, 72, 74, 75, 76,
+    186, 222, 13, 16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 38, 16, 17, 91, 18, 32, 18,
+    37, 40, 39, 17];
+const body = document.querySelector('body');
+const wrapper = document.createElement('div');
+const outputText = document.createElement('textarea');
+const keyboard = document.createElement('div');
+const keyEvents = {
+    shift() {
+        if (shiftTarget) return;
+        imitateShift();
+    },
 
-// Input: arr = [2, 3, 4, 6, 7]
-// Output: 7
+    capslock() {
+        imitateCapsLock();
+    },
 
-function largestOdd(arr) {
-    let largestOddNumber = 0;
-    arr.forEach(element => {
-        if (element % 2 && element > largestOddNumber) { largestOddNumber = element }
-    });
-    return (largestOddNumber == 0) ? "No odd numbers found" : largestOddNumber;
+    alt() {
+        altTarget = true;
+        if (ctrlTarget) {
+            language = (language === 'eng') ? 'rus' : 'eng';
+            insertKeyboardLayout(keyboardLayout[language]);
+            localStorage.language = language;
+        }
+    },
+
+    ctrl() {
+        ctrlTarget = true;
+        if (altTarget) {
+            language = (language === 'eng') ? 'rus' : 'eng';
+            insertKeyboardLayout(keyboardLayout[language]);
+            localStorage.language = language;
+        }
+    },
+
+    space() {
+        showKeyboardOutput(' ');
+    },
+
+    enter() {
+        showKeyboardOutput('\n');
+    },
+
+    tab() {
+        showKeyboardOutput('\t');
+    },
+
+    del() {
+        if (outputText.value.length === outputText.selectionEnd
+            && outputText.selectionEnd === outputText.selectionStart) return;
+        if (outputText.value.length !== outputText.selectionEnd
+            && outputText.selectionEnd === outputText.selectionStart) {
+            outputText.setRangeText('', outputText.selectionStart, outputText.selectionEnd + 1, 'end');
+        } else { showKeyboardOutput(''); }
+    },
+
+    backspace() {
+        if (outputText.selectionStart === 0 && outputText.selectionEnd === outputText.selectionStart) {
+            return;
+        }
+        if (outputText.selectionEnd === outputText.selectionStart) {
+            outputText.setRangeText('', outputText.selectionStart - 1, outputText.selectionEnd, 'end');
+        } else { showKeyboardOutput(''); }
+    },
+
+    default(activeKeyValue) { showKeyboardOutput(activeKeyValue); },
+};
+let language = 'eng';
+let ctrlTarget = false;
+let altTarget = false;
+let shiftTarget = 0;
+let uppercase = false;
+
+function keyboardInit() {
+    wrapper.classList.add('wrapper');
+    body.append(wrapper);
+    outputText.classList.add('output');
+    wrapper.append(outputText);
+    keyboard.classList.add('keyboard');
+    wrapper.append(keyboard);
+
+    if (!localStorage.language) {
+        localStorage.setItem('language', 'eng');
+    } else { language = localStorage.language; }
+
+    drawKeyboard(keyboardLayout[language]);
 }
 
-// 2
-// Let s be a string that contains a sequence of decimal numbers separated by commas, e.g., s = '1.23,2.4,3.123'. Write a function sequenceSum(s) that prints the sum of the numbers in s.
-// Input: s = '1.1, 2.1, 3.9'
-// Output: 7.1
-
-function sequenceSum(s) {
-    return s.split(',').reduce((sum, current) => sum += +current, 0);
-}
-
-// 3
-// Write a function vowelsNumber(s) that accepts string s as a parameter and counts up the number of vowels contained in the string. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'.
-// Input: s = 'azcbobobegghakl'
-// Output: 5
-
-// Input: s = 'AzcbobObEgghaKL'
-// Output: 5
-
-function vowelsNumber(s) {
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
-    let counter = 0;
-    for (let i = 0; i < s.length; i++) {
-        if (vowels.includes(s[i].toLowerCase())) counter++;
+function drawKeyboard(keys) {
+    for (let i = 0; i < keys.length; i += 1) {
+        const key = document.createElement('p');
+        key.classList.add('key');
+        key.setAttribute('data-key-code', keyCodes[i]);
+        keyboard.append(key);
     }
-    return counter;
+    insertKeyboardLayout(keyboardLayout[language]);
 }
 
-// console.log(vowelsNumber('AzcbobObEgghOOOlllaaEEyuioKL'));
+function insertKeyboardLayout(layout) {
+    const buttons = [...keyboard.querySelectorAll('.key')];
 
-// 4
-// Write a function  contains(str1, str2) that accepts two strings str1 and str2 as arguments and returns a boolean True if either string occurs anywhere in the other, and False otherwise. Function should be case insensitive.
-// Input: s1 = 'abcd', s2 = 'bc'
-// Output: True
-
-// Input: s1 = 'ABcd', s2 = 'bC'
-// Output: True
-
-function contains(str1, str2) {
-    str1 = str1.toLowerCase();
-    str2 = str2.toLowerCase();
-    if (str1.length >= str2.length) {
-        return str1.includes(str2)
-    } else { return str2.includes(str1) }
-}
-
-
-// console.log(contains('bCD', 'ABcd'));
-// 5
-// Write a function isPalindrome(s) that accepts string s as an argument and returns a boolean True if string reads the same way backwards and forwards. The function should be case and spaces insensitive.
-// Input: s = ‘topot’
-// Output: True
-
-// Input: s = ‘TopOt’
-// Output: True
-
-// Input: s = ‘to pot’
-// Output: True
-
-// Input: s = ‘ropot’
-// Output: False
-
-function isPalindrome(s) {
-    const startString = s.replace(/(\s)+/g, '').toLowerCase();
-    const invertedString = startString.split('').reverse().join('');
-    console.log(startString);
-    console.log(invertedString);
-    return startString == invertedString;
-}
-
-
-// 6
-// Write a function findExtremeDivisors(n1, n2). n1 and n2 should be positive ints. The function should return a string “Error: n1 and n2 should be positive ints!” if either n1 or n2 <= 0.
-// The function returns an array of two elements containing the smallest common divisor > 1 and the largest common divisor of n1 and n2.
-// If the are no common divisors, the function should return a string "There are no common divisors".
-// Input: n1 = 12, n2 = 6
-// Output: [2, 6]
-
-// Input: n1 = 21, n2 = 7
-// Output: [7, 7]
-
-function findExtremeDivisors(n1, n2) {
-    if (n1 <= 0 || n2 <= 0) { return 'Error: n1 and n2 should be positive ints!' }
-    const arr = [];
-    const smalestNumber = (n1 >= n2) ? n2 : n1;
-    console.log(smalestNumber)
-    for (let i = 2; i <= smalestNumber; i++) {
-        if (n1 % i == 0 && n2 % i == 0) arr.push(i);
+    for (let i = 0; i < layout.length; i += 1) {
+        if (Array.isArray(layout[i])) {
+            buttons[i].innerText = layout[i][0];
+        } else if (uppercase && layout[i].length === 1) {
+            buttons[i].innerText = layout[i].toUpperCase();
+        } else { buttons[i].innerText = layout[i]; }
+        switch (true) {
+            case (layout[i] === 'Backspace' || layout[i] === 'Enter' || layout[i] === 'Shift' || layout[i] === 'CapsLock'):
+                buttons[i].classList.add('wide-key');
+                break;
+            case (layout[i] === 'Tab' || layout[i] === 'Alt'):
+                buttons[i].classList.add('middle-key');
+                break;
+            case (layout[i] === ' '):
+                buttons[i].classList.add('space-key');
+                break;
+            default: break;
+        }
     }
-    if (arr.length > 2) arr.splice(1, arr.length - 2)
-
-    return arr;
 }
 
-
-// 7
-// Write a function intersect(L1, L2). L1 and L2 are arrays. The function returns an array that is the intersection of L1 and L2.
-// Input: L1 = [1, 2, 'a', 'c'], L2 = [1, 'b', 'c', 'd', 'e', 2]
-// Output: [1, 2, 'c']
-
-function intersect(L1, L2) {
-    const arr = [];
-    const largestArray = (L1.length >= L2.length) ? L1 : L2;
-    const lessArray = (L1.length >= L2.length) ? L2 : L1;
-    for (let i = 0; i < largestArray.length; i++) {
-        if (lessArray.includes(largestArray[i])) arr.push(largestArray[i])
-    }
-    return arr;
-}
-
-// 8
-// Write a method called indexOfMax(arr) that takes an array arr of integers and returns the index of the largest element.
-// Input: arr = [1, 2, 3, 4, 1.2, 7, 1.4342, 2.222]
-// Output: 5
-
-function indexOfMax(arr) {
-    const max = Math.max.apply(null, arr);
-    return arr.indexOf(max);
-}
-
-// 9
-// Write a method called letterHist(s) that takes a string s as a parameter and returns a string as a histogram of the letters in the string s. The zeroth element of the histogram should contain the number of a’s in the string (upper- and lowercase); the 25th element should contain the number of z’s. Your solution should only traverse the string once.
-
-// Input: s = 'cabAccCCde'
-// Output:
-// a = 2
-// b = 1
-// c = 5
-// d = 1
-// e = 1
-// f = 0
-// g = 0
-// ...
-// z = 0
-
-// 10
-// Write a recursive method named isPalindrome(s) that takes a String s as a parameter and returns a boolean indicating whether the word is a palindrome. The function should be case and spaces insensitive.
-
-// Input: s = ‘topot’
-// Output: True
-
-// Input: s = ‘to pot’
-// Output: True
-
-// Input: s = ‘TopOt’
-// Output: True
-
-// Input: s = ‘ropot’
-// Output: False
-
-
-
-// ONE TASK TO RULE THEM ALL
-// Given a matrix, write a function clockwiseSpiral(matrix) that takes the matrix as an input and prints it in clockwise spiral form. See the following examples. 
-
-// Examples:
-// Input:
-// [[1, 2, 3, 4],
-// [5, 6, 7, 8],
-// [9, 10, 11, 12],
-// [13, 14, 15, 16]]
-
-// Output:
-// 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+keyboardInit();
